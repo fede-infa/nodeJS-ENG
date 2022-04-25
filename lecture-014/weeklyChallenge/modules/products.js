f8e755f3136254e3eeeac01f473d66eb203f27d4
Federico Infantino <infantino.fede@gmail.com>
2022-04-25 18:09:30 +0200

    update folders, substract weeklyChallenge to his own folder

--- lecture-014/weeklyChallenge/modules/products.js
+++
@@ -1,9 +0,0 @@
-class Product{
-    constructor(title, price, thumbnail){
-        this.title = title,
-        this.price = price,
-        this.thumbnail = thumbnail
-    }    
-}
-
-module.exports = Product;
