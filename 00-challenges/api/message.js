492593ad933b1ef72a5da6a0bed231b9a2962f58
Federico Infantino <infantino.fede@gmail.com>
2022-05-10 10:55:48 +0200

    deprecated code deleted, fixed welcome message in /login

--- 00-challenges/api/message.js
+++ 00-challenges/api/message.js
@@ -1,22 +1,5 @@
 import MessageMongoDB from '../db/message.js'
 
-/* --------------------- Message Normalization ---------------------------- */
-/* import { normalize, schema, denormalize } from 'normalizr'
-// import util from 'util'
-
-
-// Author schema normalizr
-const schemaAuthor = new schema.Entity('author',{},{idAttribute: 'email'});
-
-// Message schema normalizr
-const schemaMenssage = new schema.Entity('post', {
-    author: schemaAuthor
-},{idAttribute: '_id'})
-
-// Post schema normalizr
-const schemaPost = new schema.Entity('posts', {
-  mensajes: [schemaMenssage]
-},{idAttribute: 'id'}) */
 /* ---------------------------------------------------------------------------- */
 
 class Message {
@@ -27,17 +10,6 @@ class Message {
 
     async getAll() {
         try {
-            /* let messages = await this.messageMongoDB.read()
-            print(messages)
-            let messageId = { 
-                id: 'messages', 
-                messages : messages.map( message => ({...message._doc}))
-            }
-            console.log(messageId);
-            print(messageId)
-            let messageIdN = normalize(messageId, schemaPost)
-            print(messageIdN)
-            return messageIdN; */
             return this.messageMongoDB.read();
         }
         catch {
--- 00-challenges/public/js/chat.js
+++ 00-challenges/public/js/chat.js
@@ -1,36 +1,3 @@
-/* --------------------- Message denormalization ---------------------------- */
-// Author schema normalizr
-/* const schemaAuthor = new normalizr.schema.Entity('author',{},{idAttribute: 'email'});
-
-// Message schema normalizr
-const schemaMenssage = new normalizr.schema.Entity('post', {
-    author: schemaAuthor
-},{idAttribute: '_id'})
-
-// Post schema normalizr
-const schemaPost = new normalizr.schema.Entity('posts', {
-  messages: [schemaMenssage]
-},{idAttribute: 'id'}) */
-/* ----------------------------------------------------------------------------- */
-
-/* ----------------------------------------------------------------- */
-/* socket.on('messages', function(messageNormalized) { 
-
-  let messageNormalizedSize = JSON.stringify(messageNormalized).length
-  console.log(messageNormalized, messageNormalizedSize);
-
-  let messageDenormalized = normalizr.denormalize(messageNormalized.result, schemaPost,messageNormalized.entities)
-  console.log(messageDenormalized);
-
-  let messageDenormalizedSize = JSON.stringify(messageDenormalized).length
-  console.log(messageDenormalized, messageDenormalizedSize);
-
-  let percentCompressed = parseInt((messageNormalizedSize * 100) / messageDenormalizedSize)
-  console.log(`Compression percent: ${percentCompressed}%`)
-  document.getElementById('compresion-info').innerText = percentCompressed
-
-  render(messageDenormalized.messages);
-}); */
 
 socket.on('messages', messages =>{
   render(messages)
